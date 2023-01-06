3fa9887f73822433d5f8a4c479d5779d974e8406
Jeremy-Doucet <jeremy.doucet93@gmail.com>
2023-01-06 16:20:47 -0600

    Finished day 6

--- 2022/day_06/index.test.ts
+++ 2022/day_06/index.test.ts
@@ -5,14 +5,46 @@ describe('Day 06', () => {
   describe('Part 1', () => {
     test('It returns the expected value', () => {
       const input = readFileSync("input.test.txt", "utf-8").toString()
-      expect(part1(input)).toBe(157)
+      expect(part1(input)).toBe(7)
+    })
+
+    test('It returns the expected value', () => {
+      expect(part1('bvwbjplbgvbhsrlpgdmjqwftvncz')).toBe(5)
+    })
+
+    test('It returns the expected value', () => {
+      expect(part1('nppdvjthqldpwncqszvftbrmjlhg')).toBe(6)
+    })
+
+    test('It returns the expected value', () => {
+      expect(part1('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg')).toBe(10)
+    })
+
+    test('It returns the expected value', () => {
+      expect(part1('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw')).toBe(11)
     })
   })
 
   describe('Part 2', () => {
     test('It returns the expected value', () => {
       const input = readFileSync("input.test.txt", "utf-8").toString()
-      expect(part2(input)).toBe(false)
+      expect(part2(input)).toBe(19)
+    })
+
+    test('It returns the expected value', () => {
+      expect(part2('bvwbjplbgvbhsrlpgdmjqwftvncz')).toBe(23)
+    })
+
+    test('It returns the expected value', () => {
+      expect(part2('nppdvjthqldpwncqszvftbrmjlhg')).toBe(23)
+    })
+
+    test('It returns the expected value', () => {
+      expect(part2('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg')).toBe(29)
+    })
+
+    test('It returns the expected value', () => {
+      expect(part2('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw')).toBe(26)
     })
   })
 })
--- 2022/day_06/index.ts
+++ 2022/day_06/index.ts
@@ -1,14 +1,23 @@
 import { readFileSync } from "fs";
+import { chain, slice } from "lodash";
 
 const input = readFileSync("input.txt", "utf-8");
 
 export function part1(data) {
-  console.log('...');
+  return chain(data)
+    .findIndex((_char, idx, arr) => {
+      return [...new Set(slice(arr, idx - 3, idx + 1))].length === 4;
+    }, 3)
+    .value() + 1
 }
 console.log(`Part 1: ${part1(input)}`);
 
 export function part2(data) {
-  console.log('...');
+  return chain(data)
+  .findIndex((_char, idx, arr) => {
+    return [...new Set(slice(arr, idx - 13, idx + 1))].length === 14;
+  }, 13)
+  .value() + 1
 }
 console.log(`Part 2: ${part2(input)}`);
 
