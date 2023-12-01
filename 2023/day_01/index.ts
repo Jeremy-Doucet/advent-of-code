ef879777e7e68fafd91b317234e5cf4a5db858ee
Jeremy-Doucet <jeremy.doucet93@gmail.com>
2023-12-01 10:12:52 -0600

    2023 day 1

--- 2023/day_01/index.ts
+++ 2023/day_01/index.ts
@@ -1,16 +1,62 @@
 import { readFileSync } from "fs";
+import { chain } from "lodash";
 
 const input = readFileSync("input.txt", "utf-8");
+const words = "one|two|three|four|five|six|seven|eight|nine";
+const numberDict = words.split("|").reduce((acc, word, index) => {
+  acc[word] = `${index + 1}`;
+  return acc;
+}, {} as Record<string, string>);
 
 export function part1(data) {
-  console.log('...');
+  const total = chain(data)
+    .split("\n")
+    .map((line) => {
+      const firstValue = line.match(/\d/);
+      const lastValue = line.match(/.*(\d)/);
+
+      if (!firstValue || !lastValue) {
+        return "";
+      }
+
+      return `${firstValue[0]}${lastValue[1]}`;
+    })
+    .filter((line) => !!line)
+    .map((line) => parseInt(line))
+    .sum()
+    .value();
+
+  return total;
 }
 console.log(`Part 1: ${part1(input)}`);
 
 export function part2(data) {
-  console.log('...');
+  const total = chain(data)
+    .split("\n")
+    .map((line) => {
+      const firstValue = line.match(new RegExp(`\\d|${words}`));
+      const lastValue = line.match(new RegExp(`.*(\\d|${words})`));
+
+      if (!firstValue || !lastValue) {
+        return "";
+      }
+
+      return `${toNumber(firstValue[0])}${toNumber(lastValue[1])}`;
+    })
+    .map((line) => parseInt(line))
+    .sum()
+    .value();
+
+  return total;
 }
 console.log(`Part 2: ${part2(input)}`);
 
 // Private functions
 
+function toNumber(str: string) {
+  const val = parseInt(str);
+
+  if (!Number.isNaN(val)) return val;
+
+  return parseInt(numberDict[str]);
+}
--- _template_files/index.ts
+++ _template_files/index.ts
@@ -3,14 +3,17 @@ import { readFileSync } from "fs";
 const input = readFileSync("input.txt", "utf-8");
 
 export function part1(data) {
-  console.log('...');
+  console.log("...");
+
+  return false;
 }
 console.log(`Part 1: ${part1(input)}`);
 
 export function part2(data) {
-  console.log('...');
+  console.log("...");
+
+  return false;
 }
 console.log(`Part 2: ${part2(input)}`);
 
 // Private functions
-
