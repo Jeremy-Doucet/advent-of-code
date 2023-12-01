c0da16ac9ff4462966e838baafd5abc081f52a96
Jeremy-Doucet <jeremy.doucet93@gmail.com>
2023-12-01 09:19:48 -0600

    init 2023

---
+++ 2023/day_20/index.ts
@@ -0,0 +1,16 @@
+import { readFileSync } from "fs";
+
+const input = readFileSync("input.txt", "utf-8");
+
+export function part1(data) {
+  console.log('...');
+}
+console.log(`Part 1: ${part1(input)}`);
+
+export function part2(data) {
+  console.log('...');
+}
+console.log(`Part 2: ${part2(input)}`);
+
+// Private functions
+
